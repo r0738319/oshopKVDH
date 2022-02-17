879a8f82c471db372fd8e194ad5c9bf68dacde86
r0738319 <r0738319@student.thomasmore.be>
2022-02-18 00:28:04 +0100

    Project: getting started

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAT7r0VSiUUeDm_VdYHoz_SmyElUiymTms",
+    authDomain: "oshopkvdhfirebase.firebaseapp.com",
+    projectId: "oshopkvdhfirebase",
+    storageBucket: "oshopkvdhfirebase.appspot.com",
+    messagingSenderId: "436879697449",
+    appId: "1:436879697449:web:13fbefdaea49a86fdd1e38"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAT7r0VSiUUeDm_VdYHoz_SmyElUiymTms",
+    authDomain: "oshopkvdhfirebase.firebaseapp.com",
+    projectId: "oshopkvdhfirebase",
+    storageBucket: "oshopkvdhfirebase.appspot.com",
+    messagingSenderId: "436879697449",
+    appId: "1:436879697449:web:13fbefdaea49a86fdd1e38"
+  }
+  
 };
 
 /*
